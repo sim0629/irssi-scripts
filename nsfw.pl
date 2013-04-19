f0a610007aec024e1248df11145439d959dc9803
sgm <sim0629@gmail.com>
2013-04-19 18:21:33 +0900

    [nsfw] nsfw.neria.kr

--- nsfw.pl
+++ nsfw.pl
@@ -1,9 +1,10 @@
 use Irssi;
 use Irssi::Irc;
+use URI::Escape;
 
 sub nsfw {
     my ($url) = @_;
-    return $url;
+    return uri_escape($url);
 }
 
 sub event_privmsg {
@@ -11,7 +12,7 @@ sub event_privmsg {
     my ($target, $text) = split(/ :/, $data, 2);
     return if($target !~ /^#neria/i);
     if($text =~ /(https?:\/\/\S+)/) {
-        my $test = "MSG $target ".nsfw($1).".";
+        my $test = "MSG $target http://nsfw.neria.kr/?q=".nsfw($1);
         $server->command($test);
     }
 }
