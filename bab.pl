657cf337652b3f09d640dc543192b8d851f23566
sgm <sim0629@gmail.com>
2014-09-24 16:59:56 +0900

    [점심] 저녁 기능 추가하고 밥으로 변경

--- bab.pl
+++ bab.pl
@@ -5,6 +5,8 @@ use LWP::UserAgent;
 use Mojo::DOM;
 
 sub menu {
+  my ($type) = @_;
+  my ($x, $y) = $type == 0 ? (3, 4) : (6, 7);
   my $request = HTTP::Request->new(GET => "http://mini.snu.kr/cafe/today/jk");
   my $ua = LWP::UserAgent->new;
   $ua->agent("Mozilla/5.0");
@@ -13,8 +15,8 @@ sub menu {
   my $response_string = $response->decoded_content;
   my $dom = Mojo::DOM->new;
   $dom->parse($response_string);
-  my $m301 = $dom->at("div#main > table > tr:nth-child(3)")->all_text;
-  my $m302 = $dom->at("div#main > table > tr:nth-child(4)")->all_text;
+  my $m301 = $dom->at("div#main > table > tr:nth-child(${x})")->all_text;
+  my $m302 = $dom->at("div#main > table > tr:nth-child(${y})")->all_text;
   return $m302."\n".$m301;
 }
 
@@ -22,10 +24,11 @@ sub event_privmsg {
   my ($server, $data, $nick, $address) = @_;
   my ($target, $text) = split(/ :/, $data, 2);
   $target = $nick if($target !~ /^#/);
-  return unless($text =~ /점심\?/);
-  my ($m1, $m2) = split("\n", menu);
-  $server->command("MSG ${target} ${m1}");
-  $server->command("MSG ${target} ${m2}");
+  my ($m1, $m2) = ("", "");
+  ($m1, $m2) = split("\n", menu(0)) if $text =~ /점심\?/;
+  ($m1, $m2) = split("\n", menu(1)) if $text =~ /저녁\?/;
+  $server->command("MSG ${target} ${m1}") if $m1;
+  $server->command("MSG ${target} ${m2}") if $m2;
 }
 
 if(caller) {
@@ -33,6 +36,8 @@ if(caller) {
   Irssi::signal_add("event privmsg", "event_privmsg");
 }else {
   binmode(STDOUT, ":utf8");
-  print menu;
+  print menu(0);
+  print "\n";
+  print menu(1);
   print "\n";
 }
